4cf6884afccc83bca4193e96fda7e3bd0619f23e
Jason Hallen <hallenj@gmail.com>
2026-07-03 11:10:11 -0500

    dynamic header and back to top button

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -38,20 +38,90 @@ document.addEventListener("DOMContentLoaded", () => {
 
   navContainer.addEventListener('transitionend', (event) => {
     if (event.propertyName === 'transform') {
-        navContainer.classList.remove('animate');
-        if (!navContainer.classList.contains('menu-open')) {
-            navLinks.style.setProperty('right', '100vw');
-            body.classList.remove('body-menu-open')
-        }
-        console.log('transition done!');
+      navContainer.classList.remove('animate');
+      if (!navContainer.classList.contains('menu-open')) {
+        navLinks.style.setProperty('right', '100vw');
+        body.classList.remove('body-menu-open')
+      }
+      console.log('transition done!');
     }
-});
+  });
 
   // Optional: Auto-close the menu drawer if a user clicks an internal link choice
-//   const navLinks = navContainer.querySelectorAll('.nav-links a');
-//   navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//       navContainer.classList.remove('menu-open');
-//     });
-//   });
+  //   const navLinks = navContainer.querySelectorAll('.nav-links a');
+  //   navLinks.forEach(link => {
+  //     link.addEventListener('click', () => {
+  //       navContainer.classList.remove('menu-open');
+  //     });
+  //   });
+});
+
+const backToTop = document.getElementById("backToTop");
+const header = document.querySelector("header");
+const threshold = 60;
+let lastScrollY = window.scrollY;
+
+function handleScroll() {
+  updateHeader();
+  updateBackToTop();
+}
+
+function updateHeader() {
+  const currentScrollY = window.scrollY;
+  const diff = currentScrollY - lastScrollY;
+
+  if (Math.abs(diff) < threshold)
+    return;
+
+  // Always show the header near the top
+  if (currentScrollY < 20) {
+    header.classList.remove("hidden");
+  }
+
+  // User is scrolling down
+  else if (currentScrollY > lastScrollY) {
+    header.classList.add("hidden");
+  }
+
+  // User is scrolling up
+  else {
+    header.classList.remove("hidden");
+  }
+
+  lastScrollY = currentScrollY;
+}
+
+function updateBackToTop() {
+  const scrollTop = window.scrollY;
+
+  const scrollHeight =
+    document.documentElement.scrollHeight -
+    window.innerHeight;
+
+  // Don't show on short pages
+  if (scrollHeight < 1000) {
+    backToTop.classList.remove("visible");
+    return;
+  }
+
+  const percent = scrollTop / scrollHeight;
+
+  if (percent > 0.30) {
+    backToTop.classList.add("visible");
+  } else {
+    backToTop.classList.remove("visible");
+  }
+}
+
+window.addEventListener("scroll", () => {
+  handleScroll();
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
 });
